8c3fa72c8d4fe7aad837d09287133cd3a4e967e4
Michael <delamarre.michael.pro@gmail.com>
2026-07-17 16:54:12 +0200

    Refactoring : Centralisation de la récupération et gestion des données dans un DataService.

---
+++ src/app/core/services/data.service.ts
@@ -0,0 +1,30 @@
+
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import {OlympicApi} from "../apis/olympic-api.api";
+import {Olympic, Olympics} from "../../models/olympic/olympic.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private readonly olympicApi = inject(OlympicApi)
+
+  getOlympics(): Observable<Olympics>{
+    return this.olympicApi.getAll()
+  }
+
+  getOlympic(country: string): Observable<Olympic | undefined> {
+    return this.olympicApi.get(country)
+  }
+
+}
+
+
+
+
+
+
+
+
