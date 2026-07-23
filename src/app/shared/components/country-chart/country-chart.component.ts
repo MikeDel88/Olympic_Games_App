cca126c45e7b6c3aa63e3b4191df284bd9d8007a
Michael <delamarre.michael.pro@gmail.com>
2026-07-23 11:06:43 +0200

    Feature: Ajout de l'accessibilité pour country-chart.component.ts

--- src/app/shared/components/country-chart/country-chart.component.ts
+++ src/app/shared/components/country-chart/country-chart.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject, input, InputSignal, OnInit} from '@angular/core';
+import {Component, computed, inject, input, InputSignal, OnInit, signal, Signal, WritableSignal} from '@angular/core';
 import Chart from 'chart.js/auto';
 import {ChartColors} from "../../styles/colors-chart.style";
 import {CountryChartDatas} from "./interfaces/country-chart-datas.interfaces";
 import {BreakpointService} from "../../responsive/breakpoint.service";
+import {AccessibilityChart} from "../../accessibility/accessibility-chart.interface";
 
 @Component({
   selector: 'app-country-chart',
@@ -11,7 +12,7 @@ import {BreakpointService} from "../../responsive/breakpoint.service";
   templateUrl: './country-chart.component.html',
   styleUrl: './country-chart.component.scss'
 })
-export class CountryChartComponent implements OnInit {
+export class CountryChartComponent implements OnInit, AccessibilityChart {
 
   countryChart!: Chart<"line", string[], number>;
 
@@ -19,11 +20,23 @@ export class CountryChartComponent implements OnInit {
 
   datas: InputSignal<CountryChartDatas> = input.required<CountryChartDatas>();
 
+  readonly activeIndex: WritableSignal<number | null> = signal(null);
+
+  readonly ariaLabel: Signal<string> = computed(() => {
+    const index = this.activeIndex();
+    if (index === null) {
+      return 'Graphique du nombre de médailles par Jeux olympics. Utilisez les flèches pour parcourir les années.';
+    }
+    const year = this.datas().years[index]
+    const medals = this.datas().medals[index]
+    return `${year} : ${medals} médailles.`;
+  });
+
   ngOnInit(): void {
     this.buildChart(this.datas());
   }
 
-  buildChart(datas: CountryChartDatas) {
+  private buildChart(datas: CountryChartDatas) {
     this.countryChart = new Chart("CountryChart", {
       type: 'line',
       data: {
@@ -46,5 +59,38 @@ export class CountryChartComponent implements OnInit {
         }
       }
     });
+  }
+
+  onKeydown(event: KeyboardEvent): void {
+    const years: number[] = this.datas().years;
+    const current: number | null = this.activeIndex();
+    switch (event.key) {
+      case 'ArrowRight':
+        event.preventDefault();
+        this.setActiveSlice(current === null ? 0 : (current + 1) % years.length);
+        break;
+      case 'ArrowLeft':
+        event.preventDefault();
+        this.setActiveSlice(current === null ? years.length - 1 : (current - 1 + years.length) % years.length);
+        break;
+      case 'Escape':
+        this.onClearActiveSlice();
+        break;
+    }
+  }
+
+  setActiveSlice(index: number): void {
+    this.activeIndex.set(index);
+    const arc = this.countryChart.getDatasetMeta(0).data[index];
+    this.countryChart.setActiveElements([{datasetIndex: 0, index}]);
+    this.countryChart.tooltip?.setActiveElements([{datasetIndex: 0, index}], {x: arc.x, y: arc.y});
+    this.countryChart.update();
+  }
+
+  onClearActiveSlice(): void {
+    this.activeIndex.set(null);
+    this.countryChart.setActiveElements([]);
+    this.countryChart.tooltip?.setActiveElements([], {x: 0, y: 0});
+    this.countryChart.update();
   }
 }
